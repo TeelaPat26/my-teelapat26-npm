9ee6e3df635294367f0436dc3a1860d4deadd668
teelapatpra <teelapat.pra@gmail.com>
2024-12-04 15:14:25 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function helloNpmFromTeelaPat26() {
+    return "Hello NPM from TeelaPat26";
+}
+
+module.exports = helloNpmFromTeelaPat26;
---
+++ test/script.js
@@ -0,0 +1,3 @@
+const helloNpmFromTeelaPat26 = require("my-teelapat26-npm");
+
+console.log(helloNpmFromTeelaPat26());
